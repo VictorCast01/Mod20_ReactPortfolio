1f11717c330869ad8400ce6a15ef227111a4669f
VictorCast01 <107225060+VictorCast01@users.noreply.github.com>
2022-09-13 22:07:09 -0400

    aboutAndResume

--- src/components/About.js
+++ src/components/About.js
@@ -7,12 +7,22 @@ export default function About() {
       <h1>About Me</h1>
 
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        My name is Victor Castellon and I was born and raised in Orlando Florida
+        for a majority of my life with the rest of the small period being in Jalisco, Mexico.
+        Throughout my time, I have grown many passions on hobbies such as: Cooking, Computers, Music, and Gaming.
+        There has been great history with every single one I have listed, however, it has been decided 
+        that it would be great to study more into Computers and find a profession that
+        can not only be a profound career to begin, but also be amusing and enjoyable.
+        </p> 
+        <p>I am always intrigued with learning new things and when it comes to computers, 
+          it never failed to amaze me. Building my PC had started my passion and have later on went more
+          into helping family and friends with assembling a new computer or even repairing old parts for them.
+          My knowledge on computer hardware is if not great then acceptable when it comes to the basics, but
+          I did not want to stop there and looked into Computer Software, where I am currently working for
+          my Bachelor's Degree in Computer Science and have completed a fullstack web development course.
+          Although I have grew very fond for Front End development compared to Back End in the course and would
+          prefer to be in the Front End, there is still so much growth in learning both branches that I hope
+          to land an even greater role for a company.
       </p>
     </div>
   );
